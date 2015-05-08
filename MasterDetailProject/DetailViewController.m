4fa6b4fb5d1592069157962d2540615a165ea0a3
Doug Voss <douglas.c.voss@gmail.com>
2015-05-08 16:33:06 -0600

    got nav bar top layout guide constraint to work

--- MasterDetailProject/DetailViewController.m
+++ MasterDetailProject/DetailViewController.m
@@ -20,21 +20,76 @@
     
     UILabel *exerciseNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 80, 320, 20)];
     exerciseNameLabel.text = self.exercise.name;
+    exerciseNameLabel.backgroundColor = [UIColor greenColor];
+    [exerciseNameLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.view addSubview:exerciseNameLabel];
     
     UITextField *weightAmountTextField = [[UITextField alloc] initWithFrame:CGRectMake(0,120,320,20)];
     weightAmountTextField.text = [NSString stringWithFormat:@"%d", self.exercise.weight];
+    weightAmountTextField.backgroundColor = [UIColor whiteColor];
+    //[weightAmountTextField setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.view addSubview:weightAmountTextField];
     
     NSMutableArray *setTextFieldArray = [NSMutableArray new];
     for (int i=0; i<self.exercise.numSets; i++) {
         UITextField *setTextField = [[UITextField alloc] initWithFrame:CGRectMake(i*50.0,180,40,40)];
         setTextField.text = @"5";
+        setTextField.backgroundColor = [UIColor whiteColor];
         [setTextFieldArray addObject:setTextField];
+        //[setTextField setTranslatesAutoresizingMaskIntoConstraints:NO];
         [self.view addSubview:setTextField];
     }
-    NSLog(@"setTextFieldArray count==%d", [setTextFieldArray count]);
+    NSLog(@"setTextFieldArray count==%d", (int)[setTextFieldArray count]);
+    
+    NSMutableArray *constraintsMutArr = [NSMutableArray new];
+    
+    NSLayoutConstraint *exerciseNameWidthConstraint =
+    [NSLayoutConstraint constraintWithItem:exerciseNameLabel
+                                 attribute:NSLayoutAttributeWidth
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.view
+                                 attribute:NSLayoutAttributeWidth
+                                multiplier:0.5
+                                  constant:0.0];
+    [constraintsMutArr addObject:exerciseNameWidthConstraint];
+
+    NSLayoutConstraint *exerciseNameVerticalPositionConstraint =
+    [NSLayoutConstraint constraintWithItem:exerciseNameLabel
+                                 attribute:NSLayoutAttributeTop
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.topLayoutGuide
+                                 attribute:NSLayoutAttributeBottom
+                                multiplier:1.0
+                                  constant:0.0];
+    [constraintsMutArr addObject:exerciseNameVerticalPositionConstraint];
+    
+    [self.view addConstraints:constraintsMutArr];
 }
+
+/*
+ + (instancetype)constraintWithItem:(id)view1
+ attribute:(NSLayoutAttribute)attr1
+ relatedBy:(NSLayoutRelation)relation
+ toItem:(id)view2
+ attribute:(NSLayoutAttribute)attr2
+ multiplier:(CGFloat)multiplier
+ constant:(CGFloat)c
+ Parameters
+ view1
+ The view for the left side of the constraint.
+ attr1
+ The attribute of the view for the left side of the constraint.
+ relation
+ The relationship between the left side of the constraint and the right side of the constraint.
+ view2
+ The view for the right side of the constraint.
+ attr2
+ The attribute of the view for the right side of the constraint.
+ multiplier
+ The constant multiplied with the attribute on the right side of the constraint as part of getting the modified attribute.
+ c
+ The constant added to the multiplied attribute value on the right side of the constraint to yield the final modified attribute.
+*/
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
